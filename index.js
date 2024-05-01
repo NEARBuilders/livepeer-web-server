11fbac55ee6fe077d3f49dacdc29921b1369a14c
stefano <stefano1990@gmail.com>
2024-05-01 21:28:00 +0200

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const videoRoutes = require("./routes/video");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", videoRoutes);
